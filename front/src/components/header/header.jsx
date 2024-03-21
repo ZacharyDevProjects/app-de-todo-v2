c766de9b1f749601f138aab8c4c47bff447139e2
maxlebro <z.joriot@gamil.com>
2024-03-21 15:50:22 +0100

    mise en place V1

---
+++ front/src/components/header/header.jsx
@@ -0,0 +1,16 @@
+function header() {
+  return (
+    <div className="fixed-top mt-1 mx-auto" style={{ width: "90vw" }}>
+      <div className=" d-flex flex-row justify-content-center align-items-start mb-3 gap-4 ">
+        <div className="fs-5 fw-bold">Postel</div>
+        <div className="fs-5">fonctionnalitées</div>
+        <div className="fs-5">solution</div>
+        <div className="fs-5">pricing</div>
+        <div className="fs-5">nous contacter</div>
+        <div className="fs-5">connexion</div>
+        <div className="fs-5">commencer</div>
+      </div>
+    </div>
+  );
+}
+export default header;
