c78791ecf36294926790d1c05a24482d064ba44e
maxlebro <z.joriot@gamil.com>
2024-03-21 15:12:45 +0100

    mise en forme de l'acceuil

--- front/src/components/acceuil/form.jsx
+++ front/src/components/acceuil/form.jsx
@@ -1,9 +1,23 @@
-function Acceuil () {
-    return (
-        <div>
-            <h1>Acceuil</h1>
+function Acceuil() {
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-center align-items-center flex-column mb-5">
+        <div className="text-wrap fs-1 mb-3" style={{ width: "50vw" }}>
+          Une application dédiée à la gestion professionnelle de vos listes de
+          tâches en ligne.
         </div>
-    );
+        <div className="text-wrap fs-4" style={{ width: "50vw" }}>
+          grace a cette application vous pourrez planifier plus facilement votre
+          travail personnelle et en équipe
+        </div>
+      </div>
+      <div className="d-flex justify-content-center align-items-center">
+        <button className="btn btn-dark" style={{height: "5vh", width: "15vw"}}>
+          Start
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Acceuil;
