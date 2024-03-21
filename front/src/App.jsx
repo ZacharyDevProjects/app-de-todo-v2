c3fe41195e480286e8ed47ba8e31910ee9b29ec4
maxlebro <z.joriot@gamil.com>
2024-03-21 15:13:11 +0100

    preparatio au route

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import Acceuil from "./components/acceuil/form";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 
 
 function App() {
   return (
     <>
-      <Acceuil/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Acceuil/>}/>
+      </Routes>
+    </Router>
     </>
   );
 }
