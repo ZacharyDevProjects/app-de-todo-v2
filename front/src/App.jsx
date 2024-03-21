ccf4386fb7e42cb4060028afd005a66da0dc7574
maxlebro <z.joriot@gamil.com>
2024-03-21 15:50:34 +0100

    route pour logIn

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,27 @@
 import "./App.css";
 import Acceuil from "./components/acceuil/form";
+import Header from "./components/header/header";
+import Login from "./components/logIn/form";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 
 
 function App() {
+  function Salut() {
+    return (
+      <>
+      <Header/>
+      <Acceuil/>
+      </>
+    );
+  }
+
   return (
     <>
     <Router>
       <Routes>
-        <Route path="/" element={<Acceuil/>}/>
+        <Route path="/" element={<Salut/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </Router>
     </>
